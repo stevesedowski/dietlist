7443bcdd07902a5052f7bbd33b0c2cfa8405fac2
Steve Sedowski <ssedowski@yahoo.com>
2014-05-16 14:44:27 -0500

    Added folders now

---
+++ js/diet.js
@@ -0,0 +1,15 @@
+function dietCtrl($scope) {
+
+	$scope.groceryList = [
+{food: 'Kale', healthy: true},
+{food: 'Spinach', healthy: true, bought: true},
+{food: 'Eggs', healthy: true, bought: true},
+{food: 'Bread', healthy: false, bought: true},
+{food: 'Pizza', healthy: false},
+{food: 'Bananas', healthy: true},
+{food: 'Oatmeal', healthy: true},
+{food: 'Sweet Potatoes', healthy: true, bought: true},
+{food: 'Avocados', healthy: true, bought: false}
+];
+}
+
